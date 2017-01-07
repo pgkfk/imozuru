41b71118bd0c66e0d7748ba14a090198652a32ca
pgkfk <kobayashi@gmail.com>
2017-01-07 19:30:30 +0900

    last

--- imozuru/views.py
+++ imozuru/views.py
@@ -5,20 +5,20 @@ import tweepy
 import sys
 # import twitter
 
-# api = ''
-# isfirst=True
-# def initialize():
-SECRET_KEY = os.environ['SECRET_KEY']
-WTF_CSRF_ENABLED = True
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-# global api
-api = tweepy.API(auth)
-# isfirst=False
+api = ''
+isfirst=True
+def initialize():
+    SECRET_KEY = os.environ['SECRET_KEY']
+    WTF_CSRF_ENABLED = True
+    CONSUMER_KEY = os.environ['CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+    ACCESS_SECRET = os.environ['ACCESS_SECRET']
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    global api
+    api = tweepy.API(auth)
+    isfirst=False
     # api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
     #                   consumer_secret=os.environ['CONSUMER_SECRET'],
     #                   access_token_key=os.environ['ACCESS_TOKEN'],
@@ -28,8 +28,8 @@ api = tweepy.API(auth)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    # if isfirst:
-    #     initialize()
+    if isfirst:
+        initialize()
 
     if request.method == 'POST':
         if 'keyword' in request.form:
