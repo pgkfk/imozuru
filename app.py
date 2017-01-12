aa494abb84c457c0fff1da3e6b3561723a991017
pgkfk <kobayashi@gmail.com>
2017-01-12 11:15:06 +0900

    soso

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ app = Flask(__name__)
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-
 api = tweepy.API(auth)
 app.secret_key = os.environ['SECRET_KEY']
 
 keyword = ''
-
 
 @app.route('/')
 def index():
@@ -26,7 +24,7 @@ def search():
     global keyword
     keyword = json_data['keyword']
     if keyword:
-        results=api.search(keyword,count=1)
+        results=api.search(keyword,count=50)
         return  json.dumps([status._json for status in results])
 
     return  jsonify(json.dumps({'status':'NO','keyword':'no keyword'}))
@@ -36,43 +34,12 @@ def pull():
     json_data = request.get_json()
     target_tweet_id = json_data['pull_id']
     target_status = api.get_status(target_tweet_id)
-    later_tweets=api.user_timeline(target_status.author.id,since_id=target_tweet_id,count=5)
-    earlier_tweets=api.user_timeline(target_status.author.id,max_id=target_tweet_id,count=5)
+    later_tweets=api.user_timeline(target_status.author.id,since_id=target_tweet_id,count=25)
+    earlier_tweets=api.user_timeline(target_status.author.id,max_id=target_tweet_id,count=25)
     result_tweets = []
     result_tweets.extend(later_tweets)
     result_tweets.extend(earlier_tweets)
     return  jsonify(json.dumps([status._json for status in result_tweets]))
-
-# @app.route('/',methods=['GET','POST'])
-# def index():
-#     if request.method == 'POST':
-#         if 'keyword' in request.form:
-#             global keyword
-#             keyword = request.form['keyword']
-#             if keyword:
-#                 return render_template(
-#                     'index.html',
-#                     keyword=keyword,
-#                     result=api.search(keyword,count=50)
-#                     )
-        # if 'pull' in request.form:
-        #     target_tweet_id = request.form['pull']
-        #     target_status = api.get_status(target_tweet_id)
-        #     # print target_tweet_id
-        #     # later_tweets=api.user_timeline(target_status.author.id,since_id=target_tweet_id,count=10)
-        #     earlier_tweets=api.user_timeline(target_status.author.id,max_id=target_tweet_id,count=1)
-        #     # return  jsonify(json.dumps([status._json for status in earlier_tweets]))
-            # return  jsonify(json.dumps({'status':'OK','user':'kb'}))
-            # return render_template(
-            #     'index.html',
-            #     keyword=keyword,
-            #     result=api.search(keyword,count=50),
-            #     later_tweets=api.user_timeline(target_status.author.id,since_id=target_tweet_id,count=10),
-            #     earlier_tweets=api.user_timeline(target_status.author.id,max_id=target_tweet_id,count=10)
-            #     )
-    # print 'hsssge'
-    # return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,10 @@
-$(function(){
-    $('button.pull').click(function(){
-        var pull_id = $(this).attr("value");
-        console.log(pull_id)
-        $.ajax({
-            url: '/pull/',
-            data: JSON.stringify({
-                'pull_id': pull_id,
-            }) ,
-            contentType:'application/json',
-            type: 'POST',
-            timeout: 10000000,
-            success: function(response){
-                var results = JSON.parse(response);
-                $('#pullResults').append('<h3>引っ張った結果...</h3>')
-                for(i=0; i<results.length; i++){
-                    var tweet = results[i];
-                    var add_row ='<blockquote class="twitter-tweet" data-conversation="none">'
-                                + '<a href="http://twitter.com/'
-                                + tweet["user"]["screen_name"]
-                                + '/status/' 
-                                + tweet["id_str"]
-                                + '">https://twitter.com/'
-                                + tweet["user"]["screen_name"]
-                                + '/status/'
-                                + tweet["id_str"]
-                                + '</a>'
-                                +'</blockquote>'
-                    $('#pullResults').append(add_row);
-                }
-                twttr.widgets.load(document.getElementById('pullResults'));
-            },
-            error: function(error){
-                console.log(error);
-                $('#hello').text('aaa')
-            }
-        });
-    });
-});
+
+window.twttr = (function (d,s,id) {
+  var t, js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+}(document, "script", "twitter-wjs"));
 
 $(function(){
     $('button.search').click(function(){
@@ -51,24 +19,16 @@ $(function(){
             timeout: 10000000,
             success: function(response){
                 var results = JSON.parse(response);
-                    $('#searchResults').append('<h3>'+ keyword +'の検索結果...</h3>')
+                $('#searchResults').append('<h3>'+ keyword +'の検索結果...</h3>')
                 for(i=0; i<results.length; i++){
                     var tweet = results[i];
-                    var add_row ='<blockquote class="twitter-tweet" data-conversation="none">'
-                                + '<a href="http://twitter.com/'
-                                + tweet["user"]["screen_name"]
-                                + '/status/' 
-                                + tweet["id_str"]
-                                + '">https://twitter.com/'
-                                + tweet["user"]["screen_name"]
-                                + '/status/'
-                                + tweet["id_str"]
-                                + '</a>'
-                                +'</blockquote>'
+                    var add_row = '<blockquote class="twitter-tweet" data-conversation="none">'
+                                +   '<a href="http://twitter.com/' + tweet["user"]["screen_name"] + '/status/' + tweet["id_str"]
+                                +   '">https://twitter.com/' + tweet["user"]["screen_name"] + '/status/' + tweet["id_str"] + '</a>'
+                                + '</blockquote>'
                                 + '<form class="form-pull">'
-                                + '<button type="pull" class="btn btn-default pull" value='
-                                + tweet["id_str"]
-                                + '>引っ張る</button></form>';
+                                +   '<button class="btn btn-default pull" type="button" value=' + tweet["id_str"] + '>引っ張る</button>'
+                                + '</form>';
                     $('#searchResults').append(add_row);
                 }
                 twttr.widgets.load(document.getElementById('searchResults'));
@@ -81,9 +41,34 @@ $(function(){
     });
 });
 
-window.twttr = (function (d,s,id) {
-  var t, js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-  js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-  return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
-}(document, "script", "twitter-wjs"));
+$(function(){
+    $(document).on('click', 'button.pull',function(){
+        var pull_id = $(this).attr("value");
+        $.ajax({
+            url: '/pull/',
+            data: JSON.stringify({
+                'pull_id': pull_id,
+            }) ,
+            contentType:'application/json',
+            type: 'POST',
+            timeout: 10000000,
+            success: function(response){
+                var results = JSON.parse(response);
+                $('#pullResults').append('<h3>引っ張った結果...</h3>')
+                for(i=0; i<results.length; i++){
+                    var tweet = results[i];
+                    var add_row = '<blockquote class="twitter-tweet" data-conversation="none">'
+                                +   '<a href="http://twitter.com/' + tweet["user"]["screen_name"] + '/status/' + tweet["id_str"]
+                                +   '">https://twitter.com/' + tweet["user"]["screen_name"] + '/status/'+ tweet["id_str"] + '</a>'
+                                + '</blockquote>'
+                    $('#pullResults').append(add_row);
+                }
+                twttr.widgets.load(document.getElementById('pullResults'));
+            },
+            error: function(error){
+                console.log(error);
+                $('#hello').text('aaa')
+            }
+        });
+    });
+});
